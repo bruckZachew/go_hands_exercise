016906f0f9171bb930e71dcc0ccb659441dba3b0
bruckZachew <b2kgizachew@gmail.com>
2024-07-04 05:14:12 -0400

    repo exercise

---
+++ go.mod
@@ -0,0 +1,5 @@
+module hands_on_exercise
+
+go 1.22.4
+
+require github.com/bruckZachew/dummer v1.0.0
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	dog "github.com/bruckZachew/dummer"
+)
+
+func main() {
+
+	fmt.Println(dog.Bark("  hello world!"))
+}
